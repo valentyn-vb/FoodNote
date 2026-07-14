71fb369bc523cf316946ae0c36a7dc7b52b9367f
Valentyn Beznosko <valikbeznosko@gmail.com>
2026-07-14 08:05:40 +0200

    install depend & add type orm in app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env', '../.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        url: config.getOrThrow<string>('DATABASE_URL'),
+        autoLoadEntities: true,
+        // Auto-sync schema in dev only!!!
+        synchronize: config.get<string>('NODE_ENV') !== 'production',
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
